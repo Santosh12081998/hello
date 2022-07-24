b7dc9749d87973248767fa60bcf3f0b1e4ab8d66
Santosh121998 <ysantosh00179@gmail.com>
2022-07-24 14:12:05 +0530

    deploying my pages

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          Welcome to Education Mantra.
+        </div>
+        <div>
+          A Great Place To Visit.
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
